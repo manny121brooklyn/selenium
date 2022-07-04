69e6d068489a41ec05c102324d9de1180992988d
manny121brooklyn <53404940+manny121brooklyn@users.noreply.github.com>
2022-07-03 22:37:40 -0400

    making changes to dev

--- assert_in.py
+++ assert_in.py
@@ -3,26 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-# class TestStringMethods(unittest.TestCase):
-#     def test_negative(self):
-#         key = 'and'
-#         container = 'mansur'
-#         message = 'key is not in container'
-#         self.assertIn(key, container, message)
-#
-# if __name__ == '__main__':
-#     unittest.main()
+class TestStringMethods(unittest.TestCase):
+    def test_negative(self):
+        key = 'and'
+        container = 'mansur'
+        message = 'key is not in container'
+        self.assertIn(key, container, message)
+
+if __name__ == '__main__':
+    unittest.main()
 
 
-# class TestStringMethods(unittest.TestCase):
-#     def test_positive(self):
-#         key = 'ansu'
-#         container = 'mansur'
-#         message = 'key is not in container'
-#         self.assertIn(key, container, message)
-#
-# if __name__ == '__main__':
-#     unittest.main()
+class TestStringMethods(unittest.TestCase):
+    def test_positive(self):
+        key = 'ansu'
+        container = 'mansur'
+        message = 'key is not in container'
+        self.assertIn(key, container, message)
+
+if __name__ == '__main__':
+    unittest.main()
 
 class TestStringMethods(unittest.TestCase):
     def test_positive(self):
