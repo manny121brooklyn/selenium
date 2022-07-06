b3ca8480b7a1dc27e49c3eaeb169c24f3278bca3
manny121brooklyn <53404940+manny121brooklyn@users.noreply.github.com>
2022-07-06 10:07:57 -0400

    making changes to dev

--- googleSearch.py
+++ googleSearch.py
@@ -4,16 +4,21 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome(executable_path='C:\selenium2\drivers\chromedriver.exe')
+driver.implicitly_wait(5)
 driver.maximize_window()
 driver.get('https://www.google.com')
+print(driver.title)
 # sch_field = driver.find_element(by=By.NAME, value='q')
 sch_field = driver.find_element(By.NAME, 'q')
 sch_field.send_keys('RD auto')
 sch_field.submit()
 time.sleep(2)
 # sch_field.send_keys(Keys.BACK_SPACE)
-sch_field.back()
+driver.back()
+print(driver.title)
+sch_field = driver.find_element(By.NAME, 'q')
 sch_field.send_keys('omation')
 
 sch_field.submit()
 time.sleep(2)
+driver.quit()
