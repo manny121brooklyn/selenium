6200e5c0380291720e96f27e81d84c8bdab57d65
manny121brooklyn <53404940+manny121brooklyn@users.noreply.github.com>
2022-07-08 21:05:59 -0400

    making changes to dev

--- youtube1.py
+++ youtube1.py
@@ -3,8 +3,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.implicitly_wait(5)
 driver.maximize_window()
 driver.get('https://www.youtube.com')
